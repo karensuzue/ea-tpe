abc3d9dc08ebd87978276964c4645f63919047a7
Jose <jgh9094@gmail.com>
2025-08-07 11:22:16 -0700

    keeps track of the best performers, removes failed eval individuals, returns results for the best performer ever seen

--- ea.py
+++ ea.py
@@ -7,8 +7,9 @@ from logger import Logger
 from individual import Individual
 from param_space import ModelParams
 from typeguard import typechecked
-from typing import List, Tuple
+from typing import List, Tuple, Dict
 from utils import eval_factory, eval_final_factory
+import copy as cp
 
 @typechecked
 class EA:
@@ -20,18 +21,22 @@ class EA:
         self.logger = logger
 
         self.param_space = param_space
-        
+
         self.population: List[Individual] = []
 
         # For debugging
         self.hard_eval_count = 0 # evaluations on the true objective
 
+        # to keep current best Individuals
+        self.best_performance = 1.0
+        self.best_performers: List[Dict] = []
+
     def get_population(self) -> List[Individual]:
         return self.population
-    
+
     def set_population(self, population: List[Individual]):
         self.population = population
-    
+
     def tournament_selection(self, population: List[Individual], performances: np.ndarray) -> Individual:
         """
         Selects a single parent using tournament selection.
@@ -39,7 +44,7 @@ class EA:
         Parameters:
             population (List[Individual]): A population of individuals to select from.
             performances (np.ndarray): A 1D array of performance values corresponding to the individuals in the population
-                (Must have the same ordering). This serves to avoid redundant computations. 
+                (Must have the same ordering). This serves to avoid redundant computations.
         Returns:
             Individual: a single parent individual.
 
@@ -56,35 +61,33 @@ class EA:
         parent = population[indices[best_tour_idx]]
         return parent
 
-
     def make_offspring(self, population: List[Individual], num_offspring: int) -> List[Individual]:
-        """ 
-        Produce offspring asexually from a given population. 
+        """
+        Produce freshly made offspring asexually from a given population.
         Parents are chosen using tournament selection.
-        
+
         Parameters:
             population (List[Individual]): A population of individuals to select from.
             num_offspring (int): The number of offspring to produce.
         """
-        offspring = []
+        offspring_params = []
         # Assume individuals are already evaluated...
         performances = np.array([ind.get_performance() for ind in self.population])
-        for _ in range(num_offspring): 
+        for _ in range(num_offspring):
             # Select a single parent using tournament selection
             parent = self.tournament_selection(population, performances)
 
-            # In-place mutation with fixing, deep copy required 
-            child = copy.deepcopy(parent)
-            self.param_space.mutate_parameters(child.get_params(), self.config.mut_rate)
+            # In-place mutation with fixing, deep copy required
+            child = copy.deepcopy(parent.get_params())
+            self.param_space.mutate_parameters(child, self.config.mut_rate)
+            offspring_params.append(child)
 
-            offspring.append(child)
-        
-        assert(len(offspring) == num_offspring)
-        return offspring
+        assert(len(offspring_params) == num_offspring)
+        return [Individual(params) for params in offspring_params]
 
     def run(self, X_train, y_train, X_test, y_test) -> None:
-        """ 
-        Run the default EA with parallelized fitness evaluations using Ray. 
+        """
+        Run the default EA with parallelized fitness evaluations using Ray.
 
         Parameters:
             X_train, y_train: The training data.
@@ -97,30 +100,28 @@ class EA:
         # Initialize population with random individuals
         self.population = [Individual(self.param_space.generate_random_parameters()) for _ in range(self.config.pop_size)]
 
-        # Make sure parameters align with scikit-learn's requirements
-        for ind in self.population:
-            self.param_space.fix_parameters(ind.get_params()) # fixes in-place 
-
-        # Create Ray ObjectRefs to efficiently share across multiple Ray tasks 
+        # Create Ray ObjectRefs to efficiently share across multiple Ray tasks
         X_train_ref = ray.put(X_train)
         y_train_ref = ray.put(y_train)
 
         # Evaluate population with Ray; 1 remote task per individual or set of hyperparameters
-        ray_evals: List[ObjectRef] = [ 
+        ray_evals: List[ObjectRef] = [
             eval_factory.remote(self.config.model, ind.get_params(), X_train_ref, y_train_ref, self.config.seed, i)
             for i, ind in enumerate(self.population) # each ObjectRef leads to a Tuple[float, int]
         ]
         # Process results as they come in
         while len(ray_evals) > 0:
-            # ray.wait() returns a list of ObjectRefs for completed results & a list of ObjectRefs for remaining jobs. 
-            # We remove the finished jobs from ray_evals by updating it to be the list of remaining jobs
-            # Here we set 'num_returns' to 1, so it returns one finished reference at a time
             done, ray_evals = ray.wait(ray_evals, num_returns = 1)
-            # Extract the only result (Tuple[float, int]) from the 'done' list
-            performance, index = ray.get(done)[0] 
+            performance, index = ray.get(done)[0]
             self.population[index].set_performance(performance)
             if self.config.debug: self.hard_eval_count += 1
-        
+
+        # Remove individuals with positive performance
+        self.remove_failed_individuals()
+        self.process_population_for_best()
+        print(f"Initial population size: {len(self.population)}")
+        print(f"Best training performance so far: {self.best_performance}")
+
         # Start evolution
         generations = (self.config.evaluations // self.config.pop_size) - 1
         for gen in range(generations):
@@ -128,9 +129,9 @@ class EA:
 
             # Produce offspring (parents may be carried over unchanged, we consider them to be new offspring regardless)
             self.population = self.make_offspring(self.population, self.config.pop_size)
-            
+
             # Evaluate offspring with Ray
-            ray_child_evals: List[ObjectRef] = [ 
+            ray_child_evals: List[ObjectRef] = [
                 eval_factory.remote(self.config.model, ind.get_params(), X_train_ref, y_train_ref, self.config.seed, i)
                 for i, ind in enumerate(self.population)
             ]
@@ -141,17 +142,23 @@ class EA:
                 self.population[index].set_performance(performance)
                 if self.config.debug: self.hard_eval_count += 1
 
-        # Get all individuals tied for best performance, randomly select one of the tied best individuals
-        self.population.sort(key=lambda o: o.get_performance()) # ascending order
-        best_performance = self.population[0].get_performance()
-        best_inds = [ind for ind in self.population if ind.get_performance() == best_performance]
-        best_ind = self.config.rng.choice(best_inds)
+            # remove individuals with positive performance
+            self.remove_failed_individuals()
+            self.process_population_for_best()
+            print(f"Population size at gen {gen}: {len(self.population)}")
+            print(f"Best training performance so far: {self.best_performance}")
+
+        # randomly select one of the tied best individuals
+        assert len(self.best_performers) > 0, "No best performers found in the population."
+        best_ind_params = self.config.rng.choice(self.best_performers)
 
         # Final scores
-        train_accuracy, test_accuracy = eval_final_factory(self.config.model, best_ind.get_params(),
+        train_accuracy, test_accuracy = eval_final_factory(self.config.model, best_ind_params,
                                                            X_train, y_train, X_test, y_test, self.config.seed)
-        best_ind.set_train_score(train_accuracy)
-        best_ind.set_test_score(test_accuracy)
+        best_ind = Individual(params=best_ind_params,
+                              performance=self.best_performance,
+                              train_score=train_accuracy,
+                              test_score=test_accuracy)
 
         self.logger.log_generation(generations, self.config.pop_size * (generations + 1), self.population, "EA")
         self.logger.log_best(best_ind, self.config, "EA")
@@ -159,5 +166,46 @@ class EA:
 
         if self.config.debug:
             print(f"Hard evaluations: {self.hard_eval_count}")
-        
+
         ray.shutdown()
+
+    # remove any individuals wiht a positive performance
+    def remove_failed_individuals(self) -> None:
+        """
+        Removes individuals with a positive performance from the population.
+        This is useful for ensuring that only individuals with negative performance are considered.
+        A positive performance indicates that the individual failed during evaluation and is not suitable for selection.
+        """
+        self.population = [ind for ind in self.population if ind.get_performance() <= 0.0]
+        if self.config.debug:
+            print(f"Removed individuals with positive performance, new population size: {len(self.population)}")
+        return
+
+    # return the best training performance from the population
+    def get_best_performance(self) -> float:
+        """
+        Returns the best training performance from the population.
+        """
+        if not self.population:
+            raise ValueError("Population is empty, cannot get best training performance.")
+        return min([ind.get_performance() for ind in self.population])
+
+    # process the current population and update self.best_performers and self.best_performance
+    def process_population_for_best(self) -> None:
+        """
+        Processes the current population and updates self.best_performers and self.best_performance.
+        """
+        current_best = self.get_best_performance()
+
+        # check if we have found a better performance
+        if current_best < self.best_performance:
+            self.best_performance = current_best
+            self.best_performers = []
+
+        # add all individuals with the current best performance to the best performers
+        for ind in self.population:
+            if ind.get_performance() == self.best_performance:
+                self.best_performers.append(cp.deepcopy(ind.get_params()))
+
+        assert len(self.best_performers) > 0, "No best performers found in the population."
+        return
