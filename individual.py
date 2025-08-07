9e76fe08255eba188578acc3dcbc094f24525e63
Jose <jgh9094@gmail.com>
2025-08-07 11:15:08 -0700

    added guarded to make sure we are not overrriding a performance

--- individual.py
+++ individual.py
@@ -5,7 +5,7 @@ from typing import Dict, Any
 @typechecked
 class Individual:
     """
-    This class encapsulates a set of hyperparameters (the "params") and stores additional information, 
+    This class encapsulates a set of hyperparameters (the "params") and stores additional information,
     such as the objective value ("performance") and expected improvement.
     """
     def __init__(self, params: Dict[str, Any], performance: float | None = None, ei: float | None = None,
@@ -14,11 +14,11 @@ class Individual:
         Parameters:
             param_space (ModelParams): This object encapsulates the parameter space and allows you to sample from it.
             performance (float): Objective function value, or the cross-validation score.
-            ei (float): Expected improvement score. 
+            ei (float): Expected improvement score.
             final_train_score (float): Final accuracy on the training set.
             final_test_score (float): Final accuracy on the testing set.
         """
-        self.performance = performance 
+        self.performance = performance
         self.ei = ei
         self.final_train_score = train_score
         self.final_test_score = test_score
@@ -28,10 +28,11 @@ class Individual:
 
     def __repr__(self):
         return f"Individual(params={self.params}, performance={self.performance})"
-    
+
     def set_performance(self, f: float) -> None:
+        assert self.performance is None, "Performance has already been set."
         self.performance = f
-    
+
     def get_performance(self) -> float:
         if self.performance is None:
             raise ValueError("Individual hasn't been evaluated.")
@@ -39,23 +40,23 @@ class Individual:
 
     def set_ei(self, ei: float) -> None:
         self.ei = ei
-    
+
     def get_ei(self) -> float:
         if self.ei is None:
             raise ValueError("Individual's Expected Improvement score has not been computed.")
         return self.ei
-    
+
     def set_train_score(self, score: float) -> None:
         self.final_train_score = score
-    
+
     def get_train_score(self) -> float:
         if self.final_train_score is None:
             raise ValueError("Individual's final training accuracy score has not been computed.")
         return self.final_train_score
-    
+
     def set_test_score(self, score: float) -> None:
         self.final_test_score = score
-    
+
     def get_test_score(self) -> float:
         if self.final_test_score is None:
             raise ValueError("Individual's final test accuracy score has not been computed.")
@@ -63,7 +64,6 @@ class Individual:
 
     def set_params(self, g: Dict[str, Any]) -> None:
         self.params = g
-    
+
     def get_params(self) -> Dict[str, Any]:
         return self.params
-    
