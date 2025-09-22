f7d1cfad731ace34cf55d1b10909758a00326d7c
Jose <jgh9094@gmail.com>
2025-09-22 15:43:46 -0700

    new fold cv num param

--- config.py
+++ config.py
@@ -17,12 +17,13 @@ class Config:
     rng: np.random.default_rng
     num_cpus: int
     mut_var: float
+    cv_k: int
 
 # class Config:
-#     def __init__(self, 
+#     def __init__(self,
 #                  seed: int = 0,
 #                  evaluations: int = 500,
-#                  pop_size: int = 50, 
+#                  pop_size: int = 50,
 #                  num_candidates: int = 10,
 #                  tour_size: int = 2,
 #                  mut_rate: float = 0.1,
@@ -30,7 +31,7 @@ class Config:
 #                  # logdir: str = 'results',
 #                  model: str = 'RF',
 #                  debug = False):
-        
+
 #         self.seed = seed
 #         self.evaluations = evaluations
 #         self.pop_size = pop_size
@@ -47,31 +48,31 @@ class Config:
 #         # This should affect the entire system
 #         random.seed(seed)
 #         np.random.seed(seed)
-    
+
 #     def get_seed(self) -> int:
 #         """ Returns the current seed. """
 #         return self.seed
-    
+
 #     def get_evaluations(self) -> int:
 #         """ Returns the number of evaluations. """
 #         return self.evaluations
-    
+
 #     def get_pop_size(self) -> int:
 #         """ Returns population size. """
 #         return self.pop_size
-    
+
 #     def get_num_candidates(self) -> int:
 #         """ Returns the number of candidate "offspring" For TPE and EA+TPE. """
 #         return self.num_candidates
-    
+
 #     def get_tour_size(self) -> int:
 #         """ Returns tournament size for EA selection. """
 #         return self.tour_size
-    
+
 #     def get_mut_rate(self) -> float:
 #         """ Returns mutation rate. """
 #         return self.mut_rate
-    
+
 #     def get_task_id(self) -> int:
 #         """ Returns current OpenML task ID. """
 #         return self.task_id
@@ -79,14 +80,14 @@ class Config:
 #     # def get_logdir(self) -> str:
 #     #     """ Returns the log directory path. """
 #     #     return self.logdir
-    
+
 #     # def get_logger(self) -> Logger:
 #     #     """ Returns the Logger object. """
 #     #     return self.logger
-    
+
 #     def get_model(self) -> str:
 #         return self.model
-    
+
 #     def get_debug(self) -> bool:
 #         """ Returns 'debug' state. """
 #         return self.debug
